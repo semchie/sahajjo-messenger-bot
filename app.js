658331c0710476f56f4101317a37ea17e2be9597
sarah.mchie <sarah.mchie@gmail.com>
2020-09-12 17:48:44 +0800

    first repository upload

---
+++ app.js
@@ -0,0 +1,313 @@
+
+/**
+ * Copyright 2017-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Messenger Platform Quick Start Tutorial
+ *
+ * This is the completed code for the Messenger Platform quick start tutorial
+ *
+ * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
+ *
+ * To run this code, you must do the following:
+ *
+ * 1. Deploy this code to a server running Node.js
+ * 2. Run `npm install`
+ * 3. Update the VERIFY_TOKEN
+ * 4. Add your PAGE_ACCESS_TOKEN to your environment vars
+ *
+ */
+
+'use strict';
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+// Imports dependencies and set up http server
+const 
+  request = require('request'),
+  express = require('express'),
+  body_parser = require('body-parser'),
+  app = express().use(body_parser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', (req, res) => {  
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    body.entry.forEach(function(entry) {
+
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender ID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+      
+    });
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', (req, res) => {
+  
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = "test_sahajjo_app";
+  
+  // Parse params from the webhook verification request
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Check if a token and mode were sent
+  if (mode && token) {
+  
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Respond with 200 OK and challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
+
+function handleMessage(sender_psid, received_message) {
+  let response;
+  
+  // Checks if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+
+    //Sets up button template
+    response = {
+        "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Choose a category",
+            //"subtitle": "Tap a button to answer.",
+            //"image_url": "https://sahajjo-test.herokuapp.com/images/profile_photo.jpg",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Remittance",
+                "payload": "remittance",
+              },
+              {
+                "type": "postback",
+                "title": "Salary",
+                "payload": "salary",
+              },
+              {
+                "type": "postback",
+                "title": "Worker rights",
+                "payload": "worker_rights",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  }
+  
+  // Send the response message
+  callSendAPI(sender_psid, response);    
+}
+
+
+function handlePostback(sender_psid, received_postback) {
+  console.log('ok')
+   let response;
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload == 'GET_STARTED') {
+
+    response = {
+        "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Choose a category",
+            //"subtitle": "Tap a button to answer.",
+            //"image_url": "https://sahajjo-test.herokuapp.com/images/profile_photo.jpg",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Remittance",
+                "payload": "remittance",
+              },
+              {
+                "type": "postback",
+                "title": "Salary",
+                "payload": "salary",
+              },
+              {
+                "type": "postback",
+                "title": "Worker rights",
+                "payload": "worker_rights",
+              }
+            ],
+          }]
+        }
+      }
+    }
+
+  } else if (payload === 'remittance') {
+    
+    response = { 
+      "text": "No remittance content yet!" 
+    }
+  } else if (payload === 'salary') {
+    
+    response = {
+        "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Choose a topic on salary issues",
+            //"subtitle": "Tap a button to answer.",
+            //"image_url": "https://sahajjo-test.herokuapp.com/images/profile_photo.jpg",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Send Money Back Home",
+                "payload": "send_money_home",
+              },
+              {
+                "type": "postback",
+                "title": "TBD",
+                "payload": "TBD",
+              },
+            ],
+          }]
+        }
+      }
+    }
+    } else if (payload === 'worker_rights') {
+
+      response = {
+        "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Choose a topic on worker rights",
+            //"subtitle": "Tap a button to answer.",
+            //"image_url": "https://sahajjo-test.herokuapp.com/images/profile_photo.jpg",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Employment Rights",
+                "payload": "employment_rights",
+              },
+              {
+                "type": "postback",
+                "title": "Passport is expiring",
+                "payload": "passport_expiring",
+              },
+              {
+                "type": "postback",
+                "title": "Received a new permit",
+                "payload": "new_permit",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } else if (payload === 'send_money_home') {
+
+      response = {
+
+        "text": 'Learn More: https://www.shahajjo.me/post/4'
+
+      }
+
+  } else if (payload === 'employment_rights') {
+
+      response = {
+
+        "text": 'Learn More: https://www.shahajjo.me/post/6'
+
+      }
+
+  } else if (payload === 'passport_expiring') {
+
+      response = {
+
+        "text": 'Learn More: https://www.shahajjo.me/post/3'
+
+      }
+
+  } else if (payload === 'new_permit') {
+
+      response = {
+
+        "text": 'https://www.shahajjo.me/post/2'
+
+      }
+
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v8.0/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
